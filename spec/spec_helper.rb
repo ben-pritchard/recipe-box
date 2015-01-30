d981b4ee4fb2c8afa7af8717094b12833cf9391c
Ben Pritchard & Arsi <student@epicodus.com>
2015-01-29 16:48:44 -0800

    Add clear class after each spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,24 @@ ENV['RACK_ENV'] = 'test'
 require("bundler/setup")
 Bundler.require(:default, :test)
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+
+RSpec.configure do |config|
+
+  config.after(:each) do
+
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+
+    Ingredient.all().each() do |ingredient|
+      ingredient.destroy()
+    end
+
+    Category.all().each() do |category|
+      category.destroy()
+    end
+    
+  end
+
+end
